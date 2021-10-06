7098097edeeb501a4d60b54028af5b7ad312816d
RawnakAra <rawnak96@gmail.com>
2021-10-06 13:59:17 +0300

    dogs

---
+++ dog pictures/dog.js
@@ -0,0 +1,11 @@
+const img = document.querySelector('#img_home'),
+       btn =document.querySelector('.btn');
+const getDogPic =async()=>{
+        const url = await (await fetch('https://dog.ceo/api/breeds/image/random')).json()
+        creatImge(url.message) 
+}
+function creatImge (img){
+    document.querySelector('#img_home').innerHTML = `<img src='${img}' width='100px' height='100px'></img>`
+}
+getDogPic()
+btn.addEventListener('click',getDogPic)
